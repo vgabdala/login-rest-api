d40e9879e3e6a1a4887d42f12de5b86295b15809
evinigu <vinicius.guimaraes@ericsson.com>
2017-08-16 16:12:47 +0100

    New route for token validation to be used by external APIs

--- app/server.js
+++ app/server.js
@@ -126,6 +126,28 @@ apiRoutes.post('/authenticate', function(req, res) {
   });
 });
 
+// Token validation for external APIs
+apiRoutes.get('/token', function(req, res){
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	if(token){
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        return res.json({ 
+	        success: true, 
+	        message: 'Valid token provided.' 
+    		});
+      }
+    });
+	}else{
+		return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+	}
+});
+
 // route middleware to verify a token
 apiRoutes.use(function(req, res, next) {
 
